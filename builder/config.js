645c60bdb5dd96a1041c7c0a080911bfa2dca367
fetis <fetis26@gmail.com>
2019-06-25 00:18:53 +0200

    footer statistics

--- builder/config.js
+++ builder/config.js
@@ -15,6 +15,7 @@ const path = require('path');
 const generateHtmlFilesForScreenshots = require('./plugins/generate-html-files-for-screenshots');
 const customMarkdownParse = require('./plugins/custom-markdown-parse');
 const validate = require('./plugins/validate');
+const statistics = require('./plugins/statistics');
 
 
 
@@ -57,6 +58,7 @@ module.exports = (isDevMode = false) => Metalsmith(__dirname)
 		[path.join('json', 'index.html'), 'data.json'],
 		[path.join('readme', 'index.html'), 'README.md'],
 	]))
+  .use(statistics())
 	.use(layouts({
 		engine: handlebars,
 		default: 'snippet.hbs',
---
+++ builder/plugins/statistics.js
@@ -0,0 +1,40 @@
+const exec = require('child_process').exec;
+
+function getSnippetsCount(metadata) {
+  return metadata.snippets.length;
+}
+
+function getAuthorsCount(metadata) {
+  const set = new Set();
+
+  metadata.snippets.forEach(snippet => set.add(snippet.author));
+
+  return set.size;
+}
+
+function getTagsCount(metadata) {
+  const tags = metadata.tags;
+  return Object.keys(tags).length;
+}
+
+module.exports = () => (files, {_metadata}, done) => {
+
+  _metadata.statistics = {
+    snippets: getSnippetsCount(_metadata),
+    authors: getAuthorsCount(_metadata),
+    tags: getTagsCount(_metadata)
+  };
+
+  exec('git rev-parse HEAD', (error, stdout) => {
+    if (error) {
+      console.warn(`Can not get Git revision`);
+      console.warn(error);
+    } else {
+      _metadata.statistics.revision = stdout.trim();
+    }
+
+    done();
+  });
+
+};
+
